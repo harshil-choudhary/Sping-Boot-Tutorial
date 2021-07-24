1cdc6d53d7fb880c494134323b3f8b87333079bc
Harshil <harshil.choudhary@paytmmall.com>
2021-07-24 19:25:50 +0530

    GET Request Finished

---
+++ src/main/java/com/harshil/springboottutorial/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.harshil.springboottutorial.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student harshil = new Student(
+                    "Harshil",
+                    "harshil@xyz.com",
+                    LocalDate.of(2568, Month.APRIL, 7),
+                    34
+            );
+
+            Student harshil1 = new Student(
+                    "Harshil1",
+                    "harshil1@xyz.com",
+                    LocalDate.of(2568, Month.MARCH, 7),
+                    31
+            );
+
+            repository.saveAll(
+                    List.of(harshil1, harshil)
+            );
+        };
+    }
+}
--- src/main/java/com/harshil/springboottutorial/student/StudentService.java
+++ src/main/java/com/harshil/springboottutorial/student/StudentService.java
@@ -1,26 +1,22 @@
 package com.harshil.springboottutorial.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.websocket.server.ServerEndpoint;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Harshil",
-                        "harshil@xyz.com",
-                        LocalDate.of(2568, Month.APRIL, 7),
-                        34
-                )
-        );
+        return studentRepository.findAll();
     }
 
 }
